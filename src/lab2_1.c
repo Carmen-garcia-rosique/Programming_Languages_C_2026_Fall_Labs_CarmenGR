c72274b92970a210bd7c9792ee0e4ba4d31c7789
Carmen-garcia-rosique <carmengarosi16@gmail.com>
2026-09-23 13:05:42 +0000

    lab2

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,17 +12,25 @@
 */
 
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+    int sum = 0;
+    for (int i= 1; i<= n; i++) {
+        sum = sum + i;
+    }
+    return sum;
 }
-
+   
 int main(void) {
     int n;
 
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
 
-    // TODO: validate input, call function, and print result
+    if (n <1 ) {
+        printf("Error: n must be at least 1.\n");
+    } else {
+        int result = sum_to_n(n);
+        printf("The sum is %d\n", result);
+    }
 
     return 0;
 }
